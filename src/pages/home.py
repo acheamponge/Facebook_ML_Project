3e190b13561d98f5729465c1e9401b51e85864f6
Emmanuel Acheampong <acheamponge@berea.edu>
2025-03-23 20:23:56 -0700

    Update home.py

--- src/pages/home.py
+++ src/pages/home.py
@@ -10,7 +10,7 @@ def write():
 
     st.markdown('<style>body{background-color: #1c1e21;} p{color: white;} .reportview-container .main .block-container {flex: 1 1 !important; max-width: 1000px !important;width: 100% !important; padding: 0 !important;} .btn-outline-secondary{border-color: white !important; color: white !important;} .bi{color: white !important} h1{color: white;} li{color: white;} .Widget>label {color: white !important;} .sidebar-content{background-color: #1c1e21 !important; background-image:none !important;}</style>', unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: white;'>A decade of Facebook Data</h1>", unsafe_allow_html=True)
-    image = Image.open('./img/1.jpg')
+    image = Image.open('./1.jpeg')
     st.write(" ")
     st.write(" ")
     st.image(image, use_column_width=True)
