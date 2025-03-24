43fe2cba8dbb096ca6ecfd2ba483262d1e600c60
Emmanuel Acheampong <acheamponge@berea.edu>
2025-03-23 20:27:51 -0700

    Update information.py

--- src/pages/information.py
+++ src/pages/information.py
@@ -8,7 +8,7 @@ from PIL import Image
 def write():
     st.markdown('<style>body{background-color: #1c1e21;} .d8{background-color: #1c1e21 !important;} body{color: white;} p{color: white;} .reportview-container .dataframe.col-header, .reportview-container .dataframe.corner, .reportview-container .dataframe.row-header {background-color: #1c1e21 !important; color: white !important;} .br {background-color: #1c1e21;!important} h2{color: white;} .reportview-container .main .block-container {flex: 1 1 !important; max-width: 1000px !important;width: 100% !important; padding: 0 !important;} .btn-outline-secondary{border-color: white !important; color: white !important;} .bi{color: white !important} h1{color: white;} li{color: white;} .Widget>label {color: white !important;} .sidebar-content{background-color: #1c1e21 !important; background-image:none !important;}</style>', unsafe_allow_html=True)
     st.markdown("<h1 style='text-align: center; color: white;'>Information Used for Recommendations</h1>", unsafe_allow_html=True)
-    image = Image.open('./img/2.JPG')
+    image = Image.open('./1.jpeg')
     st.write(" ")
     st.write(" ")
     st.image(image, use_column_width=True)
